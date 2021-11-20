5b851638af93358b2476c5d335c8518ce6282a26
kremsnita420 <duranovic.safet@gmail.com>
2021-11-20 22:03:57 +0100

    resume link

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,5 @@
-import { Typography, Container } from '@mui/material'
+import { Typography, Container, Button } from '@mui/material'
+import NextLink from 'next/link'
 import { Box } from '@mui/system'
 import Layout from '../components/layout/Layout'
 import HeadTitle from '../components/layout/HeadTitle'
@@ -10,6 +11,13 @@ export default function ContactPage() {
 				<Box sx={{ marginTop: '2rem' }}>
 					<HeadTitle title='Get in Touch' />
 				</Box>
+				<Button
+					href='https://drive.google.com/file/d/1wcz3ZNxcJw6An1YzYbM6yyBDeZypXNXH/view?usp=sharing'
+					target='_blank'
+					rel='noopener'
+					variant='outlined'>
+					PDF Resume
+				</Button>
 			</Container>
 		</Layout>
 	)
