c87ced379a130e4385e514eac4989eb60f7648dd
kremsnita420 <duranovic.safet@gmail.com>
2021-11-22 21:11:11 +0100

    bb

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,6 +3,7 @@ import NextLink from 'next/link'
 import { Box } from '@mui/system'
 import Layout from '../components/layout/Layout'
 import HeadTitle from '../components/layout/HeadTitle'
+import Image from 'next/image'
 
 export default function ContactPage() {
 	return (
@@ -16,19 +17,30 @@ export default function ContactPage() {
 				variant='outlined'>
 				PDF Resume
 			</Button>
-			<img
+			<Image
 				src='https://cdn.glitch.com/0e4d1ff3-5897-47c5-9711-d026c01539b8%2Fbddfd6e4434f42662b009295c9bab86e.gif?v=1573157191712'
-				alt='this slowpoke moves'
 				width='250'
+				height='350'
 				alt='404 image'
 			/>
-			<img
+			<Image
 				src='http://i.stack.imgur.com/SBv4T.gif'
 				alt='this slowpoke moves'
 				width='350'
+				height='350'
 			/>
-			<img src='/images/my-gif.gif' alt='this slowpoke moves' width='350' />
-			<img src='/images/my-gif2.gif' alt='this slowpoke moves' width='350' />
+			<Image
+				src='/images/my-gif.gif'
+				alt='this slowpoke moves'
+				width='350'
+				height='350'
+			/>
+			<Image
+				src='/images/my-gif2.gif'
+				alt='this slowpoke moves'
+				width='350'
+				height='350'
+			/>
 		</Layout>
 	)
 }
