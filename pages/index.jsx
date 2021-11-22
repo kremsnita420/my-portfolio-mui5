d24b43d76cb20d9b9bb42927549b08be3438e847
kremsnita420 <duranovic.safet@gmail.com>
2021-11-22 17:01:03 +0100

    vv

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import Box from '@mui/material/Box'
 import Layout from '../components/layout/Layout'
 
-import { Divider, Paper, Typography } from '@mui/material'
+import { Container, Divider, Paper, Typography } from '@mui/material'
 
 import ParticlesContainer from '../components/Particles'
 import HeroTextSvg from '../components/svgs/hero/HeroTextSvg'
@@ -14,28 +14,22 @@ export default function HomePage() {
 	return (
 		<Layout title='Home Page'>
 			<ParticlesContainer />
-			<Box
+			<Container
+				variant='outlined'
 				sx={{
-					maxWidth: 'xl',
-					marginTop: '4rem',
-				}}>
-				<Box
-					variant='outlined'
-					sx={{
-						width: '70vw',
-						padding: '2rem',
-						background: '#EE516E',
-						border: '5px solid',
-						zIndex: '99',
-						position: 'relative',
-					}}
-					elevation={3}>
-					<WebText />
-					<HeroTextSvg />
-					<FullstackTextSvg />
-					<SelfthoughtTextSvg />
-				</Box>
-			</Box>
+					width: 'md',
+					padding: '2rem',
+					background: '#EE516E',
+					border: '5px solid',
+					zIndex: '99',
+					position: 'relative',
+				}}
+				elevation={3}>
+				<WebText />
+				<HeroTextSvg />
+				<FullstackTextSvg />
+				<SelfthoughtTextSvg />
+			</Container>
 		</Layout>
 	)
 }
