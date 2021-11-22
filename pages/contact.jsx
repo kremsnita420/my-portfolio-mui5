cab8e92cec41e4feabf094d36c1dbb79302d4c79
kremsnita420 <duranovic.safet@gmail.com>
2021-11-22 21:06:39 +0100

    gif testing

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -16,6 +16,19 @@ export default function ContactPage() {
 				variant='outlined'>
 				PDF Resume
 			</Button>
+			<img
+				src='https://cdn.glitch.com/0e4d1ff3-5897-47c5-9711-d026c01539b8%2Fbddfd6e4434f42662b009295c9bab86e.gif?v=1573157191712'
+				alt='this slowpoke moves'
+				width='250'
+				alt='404 image'
+			/>
+			<img
+				src='http://i.stack.imgur.com/SBv4T.gif'
+				alt='this slowpoke moves'
+				width='350'
+			/>
+			<img src='/images/my-gif.gif' alt='this slowpoke moves' width='350' />
+			<img src='/images/my-gif2.gif' alt='this slowpoke moves' width='350' />
 		</Layout>
 	)
 }
